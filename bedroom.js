06cd314391f2fabbf5cc8e3802918d5bdf28596e
Shivaani12345 <77334932+Shivaani12345@users.noreply.github.com>
2021-08-19 14:46:18 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,38 @@
+img = "";
+status = "";
+
+function preload(){
+img = loadImage('bed.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
+  
+  function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("FF0000");
+    text("Wardrobe",320,20);
+    noFill();
+    stroke("FF0000");
+    rect(320, 0, 300, 410 );
+
+    
+}
---
+++ fruit.js
@@ -0,0 +1,37 @@
+img = "";
+status = "";
+
+function preload(){
+img = loadImage('fruit.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
+  
+  function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+function draw() {
+    
+    image(img, 0, 0, 640, 420);
+    fill("");
+    text("Apples",60,40);
+    noFill();
+    stroke("FF0000");
+    rect(40, 10, 560, 370 );
+}
